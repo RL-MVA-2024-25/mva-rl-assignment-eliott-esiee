3668a3d73593b4c5e2ac632cccae63a569bfdf4e
Eliott Vigier <150788941+eliott-esiee@users.noreply.github.com>
2025-01-11 23:11:53 +0100

    Updated train.py

--- src/train.py
+++ src/train.py
@@ -2,7 +2,9 @@ from stable_baselines3 import DQN
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 
+
 class ProjectAgent:
+
     def __init__(self):
         self.env = TimeLimit(HIVPatient(domain_randomization=False), max_episode_steps=200)
         self.model = DQN("MlpPolicy", self.env, learning_rate=1e-3, buffer_size=10000, batch_size=64, verbose=1)
@@ -15,9 +17,12 @@ class ProjectAgent:
         self.model.save(path)
 
     def load(self):
-        self.model = DQN.load("dqn_hiv_model", env=self.env)
+        self.model = DQN.load("/home/onyxia/work/mva-rl-assignment-eliott-esiee/models/dqn_hiv_model", env=self.env)
 
     def train(self, total_timesteps):
         self.model.learn(total_timesteps=total_timesteps)
+        self.save("/home/onyxia/work/mva-rl-assignment-eliott-esiee/models/dqn_hiv_model")
 
-        pass
+
+agent = ProjectAgent()
+agent.train(total_timesteps=100000)
