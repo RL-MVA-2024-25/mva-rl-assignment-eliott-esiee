7dba63c920b3e57403217b056062c4fa02d6f72a
Eliott Vigier <150788941+eliott-esiee@users.noreply.github.com>
2025-01-14 20:37:35 +0100

    Update train.py 5/9

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 from stable_baselines3 import DQN
-from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 import torch.nn as nn
@@ -14,19 +14,11 @@ def make_env():
         
     return _init
 
-
 class ProjectAgent:
 
     def __init__(self, num_envs=20):
 
-        env = SubprocVecEnv([make_env() for _ in range(num_envs)])
-        self.env = VecNormalize(
-            env, 
-            training=True,
-            norm_obs=True,
-            norm_reward=False,
-            clip_obs=10.0
-        )
+        self.env = SubprocVecEnv([make_env() for _ in range(num_envs)])
 
         self.model = DQN(
             "MlpPolicy", 
@@ -36,7 +28,7 @@ class ProjectAgent:
             target_update_interval=600,
             learning_rate=1e-3, 
             batch_size=128,
-            policy_kwargs={
+            policy_kwargs = {
                 "net_arch": [256, 256, 256, 256],  
                 "activation_fn": nn.ReLU
             },
@@ -49,19 +41,13 @@ class ProjectAgent:
 
     def save(self, path):
         self.model.save(path)
-        self.env.save("/home/onyxia/work/mva-rl-assignment-eliott-esiee/model_7/vecnormalize.pkl")
 
     def load(self):
-        env = SubprocVecEnv([make_env() for _ in range(20)])
-        self.env = VecNormalize.load("vecnormalize.pkl", env)
-        self.env.training = False
-        self.env.norm_reward = False
-
-        self.model = DQN.load("dqn_hiv_model_7", env=self.env)
+        self.model = DQN.load("dqn_hiv_model_3", env=self.env)
 
     def train(self, total_timesteps):
         self.model.learn(total_timesteps=total_timesteps)
-        self.save("/home/onyxia/work/mva-rl-assignment-eliott-esiee/model_7/dqn_hiv_model_7")
+        self.save("/home/onyxia/work/mva-rl-assignment-eliott-esiee/models/dqn_hiv_model_3")
 
 if __name__ == '__main__':
     agent = ProjectAgent()
