735c56929a336c24d4d99fe5d420875c3d937130
Eliott Vigier <150788941+eliott-esiee@users.noreply.github.com>
2025-01-14 04:24:22 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ from stable_baselines3 import DQN
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
-
+import torch.nn as nn
 
 # def make_env(seed):
 
@@ -23,7 +23,7 @@ def make_env():
 
 class ProjectAgent:
 
-    def __init__(self, num_envs=10):
+    def __init__(self, num_envs=20):
 
         # env_fns = [make_env(seed=i) for i in range(num_envs)]
         self.env = SubprocVecEnv([make_env() for _ in range(num_envs)])
@@ -32,13 +32,15 @@ class ProjectAgent:
             "MlpPolicy", 
             self.env,
             gradient_steps=8,
-            gamma=0.99,
-            target_update_interval=1000,
+            gamma=0.95,
+            target_update_interval=600,
             learning_rate=1e-3, 
-            # buffer_size=10000, 
-            batch_size=64,
-            policy_kwargs=dict(net_arch=[256, 256]),
-            tau=0.03,
+            batch_size=128,
+            policy_kwargs = {
+                "net_arch": [256, 256, 256, 256],  
+                "activation_fn": nn.ReLU
+            },
+            tau=0.02,
             verbose=1)
 
     def act(self, observation, use_random=False):
@@ -49,12 +51,12 @@ class ProjectAgent:
         self.model.save(path)
 
     def load(self):
-        self.model = DQN.load("dqn_hiv_model_2", env=self.env)
+        self.model = DQN.load("dqn_hiv_model_3", env=self.env)
 
     def train(self, total_timesteps):
         self.model.learn(total_timesteps=total_timesteps)
-        self.save("/home/onyxia/work/mva-rl-assignment-eliott-esiee/models/dqn_hiv_model_2")
+        self.save("/home/onyxia/work/mva-rl-assignment-eliott-esiee/models/dqn_hiv_model_3")
 
 if __name__ == '__main__':
     agent = ProjectAgent()
-    agent.train(total_timesteps=500000)
+    agent.train(total_timesteps=1000000)
